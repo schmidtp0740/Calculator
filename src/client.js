a3bc8ef8c54c333dfb97e3ae78e039f7c2457541
schmidtp070 <schmidtp0740@gmail.com>
2017-05-18 12:06:20 -0500

    init dir

---
+++ src/client.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './components/App';
+
+const div = document.getElementById('app');
+
+ReactDom.render(<App/>, div);
---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+    state = { counter: 0 };
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
